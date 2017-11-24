c8e1fad2944903038eca0c5df38fa9dbe473b3b2
hhli8 <963820266@qq.com>
2017-11-24 17:31:37 +0800

    get-post

--- app.js
+++ app.js
@@ -8,36 +8,12 @@ app.use(bodyParser.urlencoded({extended: true,limit: '50mb'}));
 
 //设置跨域访问
 app.all('*', function(req, res, next) {
-//res.header("Access-Control-Allow-Origin", "*");
-//res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//res.header("X-Powered-By",' 3.2.1');
-//res.header("Content-Type", "application/json;charset=utf-8");
-////res.header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-//next();
-  
-  response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "*");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token");
-        response.setHeader("Access-Control-Expose-Headers", "*");
-
-        if (request.getMethod().equals("OPTIONS")) {
-            HttpUtil.setResponse(response, HttpStatus.OK.value(), null);
-            return;
-        }
-  
-  
-//res.header('Access-Control-Allow-Origin', '*');
-//  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-//  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-//
-//  if (req.method == 'OPTIONS') {
-//      res.send(200);
-//      /make the require of options turn back quickly/
-//  } else {
-//      next();
-//  }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
 });
 
 //app.get('/',require('./routes/index.js'));
