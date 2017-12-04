cc0e0c7e4630c9451cb6f83b03548bcfd90d7375
hhli8 <963820266@qq.com>
2017-12-04 17:21:56 +0800

    ...

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,17 @@
 let express = require('express');
 let router = express.Router();
+const dns = require('dns');
+
+dns.lookup('www.weeksign.com', (err, address, family) => {
+  console.log('IP 地址: %j 地址族: IPv%s', address, family);
+});
+// IP 地址: "192.0.43.8" 地址族: IPv4
 
 router.get('/', function(req, res) {
   //res.json(req.query);
   //res.json({s:'Birds home page'});
   res.json(req.query);
+  //res.json({s:req.ip});
   
 });
 router.post('/abort', function(req, res) {
